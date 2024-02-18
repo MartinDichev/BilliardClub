2c3f5546fd01372795d5e4c90ee9d14dab3aad43
MartinDichev <dichevmartin06@gmail.com>
2024-02-18 14:23:48 +0200

    Add project files.

---
+++ BilliardClub/Models/Reservation.cs
@@ -0,0 +1,10 @@
+﻿namespace BilliardClub.Models
+{
+    public class Reservation
+    {
+        public int UserId { get; set; }
+        public DateTime DateTime { get; set; }
+        public int TableId { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ BilliardClub/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace BilliardClub.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public List<Reservation> Reservations { get; set; }
+    }
+}
